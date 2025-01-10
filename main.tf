91bbd98f1af8df0500883c32648ba6defcb398a9
bahram demo <bahramunited@gmail.com>
2025-01-10 14:49:50 +1100

    Create Resources for Networking

--- main.tf
+++ main.tf
@@ -35,3 +35,127 @@ resource "aws_iam_instance_profile" "web_hosting" {
   name = "web-hosting-profile"
   role = aws_iam_role.web_hosting.name
 }
+
+# VPC
+resource "aws_vpc" "default" {
+  cidr_block           = local.vpc.cidr_block
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = "${var.namespace}-vpc"
+  }
+}
+
+resource "aws_subnet" "public" {
+  for_each = { for index, az_name in local.vpc.azs : index => az_name }
+
+  vpc_id                  = aws_vpc.default.id
+  cidr_block              = cidrsubnet(aws_vpc.default.cidr_block, 8, (each.key + (length(local.vpc.azs) * 0)))
+  availability_zone       = each.value
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.namespace}-subnet-public-${each.key}"
+  }
+}
+
+resource "aws_subnet" "private" {
+  for_each = { for index, az_name in local.vpc.azs : index => az_name }
+
+  vpc_id            = aws_vpc.default.id
+  cidr_block        = cidrsubnet(aws_vpc.default.cidr_block, 8, (each.key + (length(local.vpc.azs) * 1)))
+  availability_zone = each.value
+
+  tags = {
+    Name = "${var.namespace}-subnet-private-${each.key}"
+  }
+}
+
+resource "aws_subnet" "private_ingress" {
+  for_each = { for index, az_name in local.vpc.azs : index => az_name }
+
+  vpc_id            = aws_vpc.default.id
+  cidr_block        = cidrsubnet(aws_vpc.default.cidr_block, 8, (each.key + (length(local.vpc.azs) * 2)))
+  availability_zone = each.value
+
+  tags = {
+    Name = "${var.namespace}-subnet-private_ingress-${each.key}"
+  }
+}
+
+resource "aws_internet_gateway" "default" {
+  vpc_id = aws_vpc.default.id
+
+  tags = {
+    Name = "${var.namespace}-internet-gateway"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.default.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.default.id
+  }
+
+  tags = {
+    Name = "${var.namespace}-route-table-public"
+  }
+}
+
+resource "aws_route_table" "private_ingress" {
+  count = length(aws_subnet.private_ingress)
+
+  vpc_id = aws_vpc.default.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.default[count.index].id
+  }
+
+  tags = {
+    Name = "${var.namespace}-route-table-private-ingress-${count.index}"
+  }
+}
+
+resource "aws_main_route_table_association" "default" {
+  vpc_id         = aws_vpc.default.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public" {
+  count = length(aws_subnet.public)
+
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "private_ingress" {
+  count = length(aws_subnet.private_ingress)
+
+  subnet_id      = aws_subnet.private_ingress[count.index].id
+  route_table_id = aws_route_table.private_ingress[count.index].id
+}
+
+resource "aws_eip" "nat_gateway" {
+  count = length(aws_subnet.public)
+
+  tags = {
+    Name = "${var.namespace}-private_ingress-nat-gateway-eip-${count.index}"
+  }
+}
+
+resource "aws_nat_gateway" "default" {
+  count = length(aws_subnet.public)
+
+  connectivity_type = "public"
+  subnet_id         = aws_subnet.public[count.index].id
+  allocation_id     = aws_eip.nat_gateway[count.index].id
+  depends_on        = [aws_internet_gateway.default]
+
+  tags = {
+    Name = "${var.namespace}-private_ingress-nat-gateway-${count.index}"
+  }
+}
